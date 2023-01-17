6cd9ffae595e0b55efc64819b4a95db4edb44b1a
Yaren Karakus <yarenkarakus007@gmail.com>
2023-01-17 19:18:04 +0300

    feat: Urls and Keys

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": process.env.REACT_APP_KEY,
+    "X-RapidAPI-Host": process.env.REACT_APP_HOST,
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_KEY;
